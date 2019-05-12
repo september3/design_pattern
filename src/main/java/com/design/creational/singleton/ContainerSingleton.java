aa3ef6b46651c065eea120537fdcb9a7370e0316
september3 <1296648354@qq.com>
2019-05-12 15:42:43 +0800

    ‘commit singleton

---
+++ src/main/java/com/design/creational/singleton/ContainerSingleton.java
@@ -0,0 +1,32 @@
+package com.design.creational.singleton;
+
+
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class ContainerSingleton {
+
+    private ContainerSingleton(){
+
+    }
+    private static Map<String,Object> singletonMap = new HashMap<String,Object>();
+
+    public static void putInstance(String key,Object instance){
+
+        if(StringUtils.isNotBlank(key) && instance != null){
+            if(!singletonMap.containsKey(key)){
+                singletonMap.put(key,instance);
+            }
+        }
+    }
+
+    public static Object getInstance(String key){
+        return singletonMap.get(key);
+    }
+
+
+}
---
+++ src/main/java/com/design/creational/singleton/EnumInstance.java
@@ -0,0 +1,25 @@
+package com.design.creational.singleton;
+
+
+public enum EnumInstance {
+    INSTANCE{
+        @Override
+        protected  void printTest(){
+            System.out.println("Geely Print Test");
+        }
+    };
+    protected abstract void printTest();
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+    public static EnumInstance getInstance(){
+        return INSTANCE;
+    }
+
+}
