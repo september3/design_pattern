5592a5f82ebf03aefdaf702a14e808fcbfb8c88b
september3 <1296648354@qq.com>
2019-05-12 15:20:08 +0800

    commit builder

---
+++ src/main/java/com/design/creational/builder/Course.java
@@ -0,0 +1,15 @@
+package com.design.creational.builder;
+
+import lombok.Data;
+
+@Data
+public class Course {
+    private String courseName;
+    private String coursePPT;
+    private String courseVideo;
+    private String courseArticle;
+
+    //question & answer
+    private String courseQA;
+
+}
